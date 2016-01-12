ee32e26dba276783c59b110f1391319182c5fa7e
QlCrisis <qlcrisis808@gmail.com>
2016-01-11 21:47:36 -0500

    deleting test projects
    deleting test projects made to configure intellij.

--- 100-Projects/IdeaProjects/test/src/otherclass.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by QLCrisis on 1/11/2016.
- */
-public class otherclass {
-}
--- 100-Projects/IdeaProjects/test/src/test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by QLCrisis on 1/11/2016.
- */
-public class test {
-    public static void main(String[] args) {
-
-    }
-}
